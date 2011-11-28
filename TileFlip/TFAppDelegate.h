f3fdc5b15b869c5833125c0a3c1afee365d9806b
Gary A. Morris <gary@mggm.net>
2011-11-27 19:58:00 -0800

    Initial Commit

---
+++ TileFlip/TFAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TFAppDelegate.h
+//  TileFlip
+//
+//  Created by Gary Morris on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TFViewController;
+
+@interface TFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) TFViewController *viewController;
+
+@end
---
+++ TileFlip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TileFlip
+//
+//  Created by Gary Morris on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TFAppDelegate class]));
+    }
+}
